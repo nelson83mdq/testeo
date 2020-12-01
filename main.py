e02c390e9146d088fa4897c589be895992991fab
= <=>
2020-12-01 18:12:57 -0300

    principio

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 from pymongo import MongoClient
+from datetime import datetime
 
+conn = MongoClient()
+db = conn['RegistroOt']
+
+
+def __getTimeStamp():
+    return (datetime.now().strftime('%d.%m.%y %X'))
+
+Tecnico = {
+    'Nombre':'',
+    'Especialidad': ''
+}
+
+Ot = {
+    'Fecha': '',
+    'Tecnico': '',
+    'Observaciones':'',
+    'NroOt':'',
+    'Materiales':[]
+}
